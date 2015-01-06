94f0008ff3beebab5aa8b52da77bbf5056689054
Lotiz Sjödin <ls222vy@student.lnu.se>
2015-01-06 22:43:37 +0000

    More css code.

--- 5-pwd/script/webDesktop.js
+++ 5-pwd/script/webDesktop.js
@@ -3,17 +3,12 @@
  var webDesktop = {
      
      init: function (){
-        // var menuButton = document.getElementById("click");
-         
-         //en reset-knapp för att återställa bakgrunden
-       //  var resetButton = document.getElementById("reset");
-        // var buttonUrl = "css/background/dotted1.png";
-         
-        // webDesktop.changeBackground(buttonUrl, resetButton);
+         var menuButton = document.getElementById("click");
+       
          
          //Gör den till en knapp
          
-        // menuButton.onclick = function(){
+         menuButton.onclick = function(){
              var desktop = document.getElementById("desktop");
              
              //Validerar eller kollar om ett fönster finns. hindrar att fler skapas.
@@ -21,8 +16,75 @@
              if (desktop.firstElementchild){
                  return false;
              }
+             
+            
+             var imageFolder = document.createElement("div");
+             imageFolder.id="imgFolder";
+             
+             
+             var top = document.createElement("div");
+             top.id ="top";
+             
+             var topImage = document.createElement("img");
+             topImage.className = "topImage";
+             topImage.setAttribute("src", "pics/windowicon.png");
+             
+             var topText = document.createElement("span");
+             topText.id = "folderText";
+             topText.appendChild(document.createTextNode("image Viewer"));
+             
+             var topClose = document.createElement("a");
+             topClose.className = "topClose";
+             topClose.setAttribute("href", "#");
+             
+             //Stäng-knappen för fönstret
+             
+             topClose.onclick = function(){
+              desktop.removeChild(imageFolder);
+              
+             }
+             
+             var closeImg = document.createElement("img");
+             closeImg.setAttribute("src", "pics/windowclose.png");
+             
+             var picContainer = document.createElement("div");
+             picContainer.id = "picContainer";
+             
+             var bottom = document.createElement("div");
+             bottom.id = "bottom";
+             
+             var loader = document.createElement("img");
+             loader.setAttribute("src", "pics/loader.gif");
+             
+             desktop.appendChild(imageFolder);
+             imageFolder.appendChild(top);
+             top.appendChild(topImage);
+             top.appendChild(topText);
+             top.appendChild(topClose);
+             imageFolder.appendChild(picContainer);
+             imageFolder.appendChild(bottom);
+             bottom.appendChild(loader);
+             
+             var url = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";
+             
+              new AjaxCon(url,function (data)){
+               
+               //Ta bort laddning-gif när anropet är färdigt.
+               bottom.removeChild(loader);
+               
+               var images = JSON.parse (data);
+               //Funktionen kollar vilket tummnagel som är störst.
+               
+              
+             }
+             
+             
+             
          }
      }
+ };
+ 
+     
  
  
- window.onload = webDesktop.init
+ window.onload = webDesktop.init;
